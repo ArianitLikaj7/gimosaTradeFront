d816563137aa9a998e5aebea4a7af343d2c4cbf3
Arianit Likaj <arianitlikaj46@gmail.com>
2025-01-06 18:52:42 +0100

    fix bug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://gimosatrade.onrender.com/api'
+  production: false,
+  apiUrl: 'http://localhost:8080/api',  // Local API URL for development
 };
